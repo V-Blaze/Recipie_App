aae372da15b656fdbf47a26de833b1fecab977df
V-Blaze <valentine6586@gmail.com>
2023-03-14 09:54:36 +0100

    Add controller methods Create, show, index, destroy, public_recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,27 @@
 class RecipesController < ApplicationController
+    before_action :authenticate_user!, only: [:index, :create, :destroy]
+
+    def index
+        @recipes = Recipe.created_by_current_user(current_user)
+    end
+
+    def show
+        
+    end
+
+    def create
+        #
+    end
+
+    def destroy
+        #
+    end
+
+    def public_recipes
+        @recipes = Recipe.public_recipes_ordered_by_newest
+    end
+
+    def recipe_params
+        params.require(:recipe).permit(:name, :description, :public, :preparation_time, :cooking_time)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  get 'public_recipes', to: 'recipes#public_recipes'
+  resources :recipes, only: [:index, :show, :create, :destroy]
+
   # Defines the root path route ("/")
   # root "articles#index"
 end

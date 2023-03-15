81bf699b44892d1052fd1abaacf15a4f20a3888f
Trust7 <simeontrust7@gmail.com>
2023-03-15 21:08:28 +0100

    Add model test for food

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+first_user = User.create(name: 'Jack Frost', email: 'jackfrost@gmail.com', password: '123456');
+
+second_user = User.create(name: 'Peter Parker', email: 'peterparker@gmail.com', password: '123456');
+
+Food.create(name: 'Turkey', measurement_unit: 'grams', price: '20.0', quantity: 4, user: first_user, user_id: first_user.id)
+Food.create(name: 'Potato', measurement_unit: 'grams', price: '5.0', quantity: 2, user: first_user, user_id: first_user.id)
+Food.create(name: 'Strawberry', measurement_unit: 'grams', price: '10.0', quantity: 10, user: sec_user, user_id: sec_user.id)
---
+++ spec/models/food_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe Food, type: :model do
+  current_user = User.first
+  subject { Food.new(name: 'Turkey', measurement_unit: 'grams', price: '20.0', quantity: 4, user: current_user) }
+
+  before { subject.save }
+
+  it 'name should be present' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'measurement unit should be present' do
+    subject.measurement_unit = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'price should be equal or great than 0' do
+    subject.price = -1
+    expect(subject).to_not be_valid
+  end
+
+  it 'Subject should be valid' do
+    expect(subject).to be_valid
+  end
+  
+end

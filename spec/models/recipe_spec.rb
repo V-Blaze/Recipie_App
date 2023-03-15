c3eb7c944b9c72b9fb257040d99f377629ce2edd
V-Blaze <valentine6586@gmail.com>
2023-03-15 11:53:58 +0100

    Write Rspec unit test fir the Recipes model

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -1,5 +1,44 @@
 require 'rails_helper'
+require 'spec_helper'
 
 RSpec.describe Recipe, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  current_user = User.first
+
+  subject {Recipe.new(name: 'Pizza', description: 'All the best description', cooking_time: 30, preparation_time: 30, public: true, user: current_user)}
+
+  before { subject.save }
+
+  it 'Name should be present' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'cooking time should be an integer' do
+    expect(subject.cooking_time).to be_a_float
+  end
+
+  it 'Preparation_time must be a positive integer' do
+    expect(subject.preparation_time).to be_positive.and be_a_float
+  end
+
+  it 'Public must be a boolean value [true or false]' do
+    expect(subject.public).to be_a_boolean
+  end
+
+  it 'should return only Recipes with the public attibute set to true' do
+    # expect(Recipe.public_recipes_ordered_by_newest.count).to eq(1)
+  end
+
+  it 'should return only Recipes with the public attibute set to true' do
+    # expect(Recipe.public_recipes_ordered_by_newest.count).to eq(1)
+  end
+
+  it 'should return only Recipes created by the current user' do
+    @recipes = Recipe.created_by_current_user(current_user)
+    current_user_id = @recipes.first.user.id
+
+    expect(current_user_id).to eq(current_user.id)
+  end
+
+
 end
